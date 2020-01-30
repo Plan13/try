2134f238424c6a0bdbca1cb6807391194f42d7c3
Plan13 <plan13@list.ru>
2020-01-30 14:41:01 +1000

    DOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&wzmrc(l2nwc7ygg^=@iwa2z5#kcn@bj)cyx)coih8hovy+yo='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'plan1313.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
